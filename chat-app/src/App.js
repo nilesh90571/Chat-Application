85eef1ca16d37838e84162eca6df8e8154a1b60f
nilesh90571 <gnilesh2008@gmail.com>
2023-09-25 12:25:27 +0530

    first commit

---
+++ chat-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Join from "./component/Join/Join"
+import Chat from "./component/Chat/Chat"
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Join />} />
+          <Route path='/chat' element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
